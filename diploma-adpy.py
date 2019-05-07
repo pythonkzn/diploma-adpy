000f9a5e86b81070b5a038edd6490c2e4dcaf638
raravilov <raravilov@gmail.com>
2019-05-07 14:12:00 +0300

    Initial commit 1. Получать токен от пользователя с нужными правами.

---
+++ diploma-adpy.py
@@ -0,0 +1,15 @@
+from modules.vkauth import VKAuth
+from modules.user import User
+
+def main():
+    get_auth = VKAuth(['friends'], '6889971', '5.95')
+    get_auth.auth()
+    print(get_auth._access_token)
+    user = User(get_auth._access_token, get_auth._user_id)
+    print(user.get_all_data(0, 0))
+
+
+
+if __name__ == "__main__":
+    main()
+
